da2ab39956d858604463e048e3d5816176114767
oscarleon5AVP <124410665+oscarleon4AVP@users.noreply.github.com>
2023-09-05 19:26:57 -0700

    05/09/23 juego de pares

---
+++ parcial1/pares.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    
+    var contador = 0;
+    $('img').click(function(e){
+        var nombre_imagen = $(this).attr('data-id')
+        $(this).attr('src','imagen/' + nombre_imagen);
+    });
+});
