61c68cb35f865c62ccecfd58c67d2a2e3f616623
oscarleon4AVP <124410665+oscarleon4AVP@users.noreply.github.com>
2023-09-06 19:18:43 -0700

    06

--- parcial1/pares.js
+++ parcial1/pares.js
@@ -1,8 +1,34 @@
 $(document).ready(function(){
-    
-    var contador = 0;
+    var carta1 = ""; var carta2 = "";
+    var par= false;
+    var total_pares = 0; 
+
     $('img').click(function(e){
+        var estado = $(this).attr('data-estado')
         var nombre_imagen = $(this).attr('data-id')
-        $(this).attr('src','imagen/' + nombre_imagen);
+
+        if(estado == "0"){
+            console.log("estado de la carta:" + estado)
+            if(carta1 == ""){
+                carta1 = $(this);
+                carta1.attr('src','imagen/' + nombre_imagen);
+                console.log("seasigno carta #1")
+            } else{
+                    carta2 = $(this);
+                    carta2.attr('src','imagen/' + nombre_imagen);
+                    console.log("seasigno carta #2")
+        }
+    }
+
+    if(carta1 != "" && carta2 != ""){
+        if(carta1.attr('data-id') == carta2.attr('data-id')){
+            par = true
+            total_pares++
+            $("#total_pares").html(total_pares)
+            carta1 = ""
+            carta2 = ""
+        }    
+    }
+    
     });
 });
